d2f5330fee9458995c7ac3b6f287bdc1d99c8ad9
Antonio Galea <antonio.galea@gmail.com>
2022-09-28 11:07:39 +0200

    lora3a-h10: better control logic for sensor buses at boot

--- boards/lora3a-h10/board.c
+++ boards/lora3a-h10/board.c
@@ -2,6 +2,7 @@
 
 #include "board.h"
 #include "cpu.h"
+#include "periph/i2c.h"
 #include "periph/gpio.h"
 
 #ifdef MODULE_SX127X
@@ -25,19 +26,19 @@ void board_init(void)
     gpio_write(TX_OUTPUT_SEL_PIN, !SX127X_PARAM_PASELECT);
 #endif /* USEMODULE_SX127X */
 
-#if defined(BME688_ACME1)
-	puts("BME688 on ACME Sensor 1\n");
-	#define BME688_ENABLE  GPIO_PIN(PA, 28)
+#if defined(ENABLE_ACME1)
+	puts("Enable ACME Sensor 1\n");
 #else
     // disable i2c port on acme sensor 1
+    i2c_deinit_pins(1);
     gpio_init(GPIO_PIN(PB, 2), GPIO_IN_PU);
     gpio_init(GPIO_PIN(PB, 3), GPIO_IN_PU);
 #endif
-#if defined(BME688_ACME2)
-	puts("BME688 on ACME Sensor 2\n");
-	#define BME688_ENABLE  GPIO_PIN(PA, 31)
+#if defined(ENABLE_ACME2)
+	puts("Enable ACME Sensor 2\n");
 #else
     // disable i2c port on acme sensor 2
+    i2c_deinit_pins(2);
     gpio_init(GPIO_PIN(PA, 4), GPIO_IN_PU);
     gpio_init(GPIO_PIN(PA, 5), GPIO_IN_PU);
 #endif
