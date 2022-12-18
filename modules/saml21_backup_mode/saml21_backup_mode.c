5feeacfc141d0f118caf3b798a0d03e495891cc8
Antonio Galea <antonio.galea@gmail.com>
2022-12-18 18:03:46 +0100

    saml21_backup_mode module, tested and with debug

--- modules/saml21_backup_mode/saml21_backup_mode.c
+++ modules/saml21_backup_mode/saml21_backup_mode.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include "board.h"
 #include "saml21_backup_mode.h"
 
@@ -5,42 +6,61 @@
 #include "periph/pm.h"
 #include "periph/rtt.h"
 
+#define ENABLE_DEBUG 0
+#include "debug.h"
+
 saml21_wakeup_cause_t saml21_wakeup_cause(void)
 {
     saml21_wakeup_cause_t cause;
 
-    switch (RSTC->RCAUSE.reg | RSTC_RCAUSE_MASK) {
+    DEBUG("RSTC->RCAUSE:   %02x\n", RSTC->RCAUSE.reg);
+    DEBUG("RSTC->BKUPEXIT: %02x\n", RSTC->BKUPEXIT.reg);
+    DEBUG("RSTC->WKCAUSE:  %02x\n", RSTC->WKCAUSE.reg & 0xff);
+    switch (RSTC->RCAUSE.reg) {
     case RSTC_RCAUSE_POR:
         cause = POWER_ON;
+        DEBUG("Cause: POWER_ON\n");
         break;
     case RSTC_RCAUSE_BOD12:
     case RSTC_RCAUSE_BOD33:
         cause = BROWN_OUT;
+        DEBUG("Cause: BROWN_OUT\n");
         break;
     case RSTC_RCAUSE_EXT:
         cause = EXTERNAL;
+        DEBUG("Cause: EXTERNAL\n");
         break;
     case RSTC_RCAUSE_WDT:
         cause = WATCHDOG;
+        DEBUG("Cause: WATCHDOG\n");
         break;
     case RSTC_RCAUSE_SYST:
         cause = SYSTEM;
+        DEBUG("Cause: SYSTEM\n");
         break;
     case RSTC_RCAUSE_BACKUP:
-        switch (RSTC->BKUPEXIT.reg | RSTC_BKUPEXIT_MASK) {
+        switch (RSTC->BKUPEXIT.reg) {
         case RSTC_BKUPEXIT_EXTWAKE:
             cause = BACKUP_EXTWAKE;
+            DEBUG("Cause: BACKUP_EXTWAKE\n");
             break;
         case RSTC_BKUPEXIT_RTC:
             cause = BACKUP_RTC;
+            DEBUG("Cause: BACKUP_RTC\n");
             break;
         case RSTC_BKUPEXIT_BBPS:
             cause = BACKUP_POWER_SWITCH;
+            DEBUG("Cause: BACKUP_POWER_SWITCH\n");
+            break;
+        default:
+            cause = BACKUP_EXTWAKE;
+            DEBUG("Cause: fallback to BACKUP_EXTWAKE\n");
             break;
         }
         break;
     default:
-        cause = RSTC_RCAUSE_POR;
+        cause = POWER_ON;
+        DEBUG("Cause: fallback to POWER_ON\n");
         break;
     }
     return cause;
@@ -48,7 +68,9 @@ saml21_wakeup_cause_t saml21_wakeup_cause(void)
 
 uint8_t saml21_wakeup_pins(void)
 {
-    return RSTC->WKCAUSE.reg && 0xff;
+    uint8_t pins = RSTC->WKCAUSE.reg & 0xff;
+    DEBUG("Pins: 0x%02x\n", pins);
+    return pins;
 }
 
 void saml21_backup_mode_enter(int wakeup_pin, int sleep_secs)
