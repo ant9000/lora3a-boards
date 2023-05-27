6ac7327815cddcc381ad60e8f717482369dd37f0
Antonio Galea <antonio.galea@gmail.com>
2023-05-27 14:15:43 +0200

    shell_extra_commands: use fram module

--- modules/shell_extra_commands/sc_fram.c
+++ modules/shell_extra_commands/sc_fram.c
@@ -5,13 +5,11 @@
 #include "od.h"
 #include "shell.h"
 
-#include "at24cxxx.h"
-#include "at24cxxx_params.h"
+#include "fram.h"
 
 #define BUFFER_SIZE     (128U)
 
-static char buffer[BUFFER_SIZE + 1];
-static at24cxxx_t at24cxxx_dev;
+static char buffer[BUFFER_SIZE];
 static int initialized = 0;
 
 static int cmd_init(int argc, char **argv)
@@ -19,16 +17,7 @@ static int cmd_init(int argc, char **argv)
     (void)argc;
     (void)argv;
 
-    printf("EEPROM size: %u byte\n", AT24CXXX_EEPROM_SIZE);
-    printf("Page size  : %u byte\n", AT24CXXX_PAGE_SIZE);
-
-    int check = at24cxxx_init(&at24cxxx_dev, &at24cxxx_params[0]);
-    if (check != AT24CXXX_OK) {
-        printf("ERROR: initialization failed (%d)\n", check);
-        return 1;
-    }
-    puts("FRAM initialized successfully");
-    initialized = 1;
+    initialized = (fram_init() == 0);
     return 0;
 }
 
@@ -47,23 +36,14 @@ static int cmd_read(int argc, char **argv)
     uint32_t pos = atoi(argv[1]);
     uint32_t count = atoi(argv[2]);
 
-    if (pos + count > AT24CXXX_EEPROM_SIZE) {
-        puts("Failed: cannot read out of FRAM bounds");
-        return 1;
-    }
-
     for(uint32_t len = 0; len <= count; len += BUFFER_SIZE) {
         uint32_t n = (count - len) > BUFFER_SIZE ? BUFFER_SIZE : (count - len);
-        int check = at24cxxx_read(&at24cxxx_dev, pos + len, buffer + len, n);
-        if (check != AT24CXXX_OK) {
-            printf("ERROR: read from FRAM failed (%d)\n", check);
+        if (fram_read(pos + len, buffer, n)) {
             return 1;
         }
+        od_hex_dump_ext(buffer, n, 0, len);
     }
-    buffer[count] = '\0';
-    printf("Data read from FRAM (%lu bytes):\n", count);
-    od_hex_dump(buffer, count, 0);
-
+    printf("\n%lu bytes read from FRAM\n", count);
     return 0;
 }
 
@@ -83,14 +63,7 @@ static int cmd_write(int argc, char **argv)
     uint8_t *data = (uint8_t *)argv[2];
     size_t len = strlen(argv[2]);
 
-    if (pos + strlen(argv[2]) > AT24CXXX_EEPROM_SIZE) {
-        puts("Failed: cannot write out of FRAM bounds");
-        return 1;
-    }
-
-    int check = at24cxxx_write(&at24cxxx_dev, pos, data, len);
-    if (check != AT24CXXX_OK) {
-        printf("ERROR: write to FRAM failed (%d)\n", check);
+    if (fram_write(pos, data, len)) {
         return 1;
     }
     printf("%d bytes written to FRAM\n", len);
@@ -105,13 +78,9 @@ static int cmd_erase(int argc, char **argv)
         return 1;
     }
 
-    int check = at24cxxx_erase(&at24cxxx_dev);
-    if (check != AT24CXXX_OK) {
-        printf("ERROR: FRAM erase failed (%d)\n", check);
+    if (fram_erase()) {
         return 1;
     }
-    puts("FRAM erased with success");
-
     return 0;
 }
 
