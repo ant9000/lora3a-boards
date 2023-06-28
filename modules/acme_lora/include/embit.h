28645b52121aa0a233c3b0bf03e89ec96acc48e3
Michele Forese <michele.forese.personal@gmail.com>
2023-06-28 16:58:27 +0200

    - removed common.h - renamed lora.h to acme_lora.h integrating de defaults

--- modules/acme_lora/include/embit.h
+++ modules/acme_lora/include/embit.h
@@ -2,24 +2,25 @@
 #define EMBIT_H
 
 #include <inttypes.h>
+#include <stddef.h>
 
 typedef struct {
-    uint16_t signature;
-    uint16_t counter;
-    uint16_t network;
-    uint8_t dst;
-    uint8_t src;
+  uint16_t signature;
+  uint16_t counter;
+  uint16_t network;
+  uint8_t dst;
+  uint8_t src;
 } embit_header_t;
 
 typedef struct {
-    embit_header_t header;
-    char *payload;
-    size_t payload_len;
-    int16_t rssi;
-    int8_t snr;
+  embit_header_t header;
+  char *payload;
+  size_t payload_len;
+  int16_t rssi;
+  int8_t snr;
 } embit_packet_t;
 
-#define EMB_HEADER_LEN  sizeof(embit_header_t)
+#define EMB_HEADER_LEN sizeof(embit_header_t)
 
 #define EMB_BROADCAST 0xff
 #define EMB_SIGNATURE 0x00e0
