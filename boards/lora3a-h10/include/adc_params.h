ff9389118ca92342015dadd926903a37f860f6a2
Antonio Galea <antonio.galea@gmail.com>
2023-07-18 21:12:11 +0200

    lora3a-h10: adc defs for saul

---
+++ boards/lora3a-h10/include/adc_params.h
@@ -0,0 +1,37 @@
+#include "board.h"
+#include "saul/periph.h"
+#include "periph/adc.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/**
+ * @brief    ADC configuration
+ */
+static const  saul_adc_params_t saul_adc_params[] =
+{
+    {
+        .name = "Vcc",
+        .line = ADC_LINE(0),
+        .res  = ADC_RES_12BIT,
+    },
+    {
+        .name = "Vpanel",
+        .line = ADC_LINE(1),
+        .res  = ADC_RES_12BIT,
+    },
+    {
+        .name = "Bandgap",
+        .line = ADC_LINE(2),
+        .res  = ADC_RES_12BIT,
+    }
+};
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* ADC_PARAMS_H */
+/** @} */
+
